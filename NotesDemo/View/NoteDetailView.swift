af3fef6cdf9baebcdfd943db3715df5ae7713641
Lukáš Schmelcer <xschme00@fit.vutbr.cz>
2020-12-11 22:11:54 +0100

    Fixed problem with notes not updating

--- NotesDemo/View/NoteDetailView.swift
+++ NotesDemo/View/NoteDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct NoteDetailView: View {
     @State var title: String = ""
@@ -13,32 +14,44 @@ struct NoteDetailView: View {
     var folder: Folder
     @Environment(\.managedObjectContext) private var viewContext
     var id: UUID?
+    var changed : UUID // Salt change to make swiftui update foreach in pointing view
     var body: some View {
         VStack{
             TextField("Názov", text: $title).font(.title)
             Divider()
             MultiLineTextField(text: $text)
             }.padding().navigationBarItems(trailing: Button(action:{
-                do{
-                    let note = Note(context: self.viewContext)
-                    note.content = self.text
-                    note.name = self.title
-                    note.date = Date()
-                    note.id = UUID()
-                    note.is_in = self.folder
-                    try self.viewContext.save()
-                }catch{
-                    print("TODO: Handle err")
+                if let id = id{
+                    let note = self.folder.wContains.filter({$0.id == id}).map({return $0})
+                    if(note.count != 0) {
+                        note[0].content = self.text
+                        note[0].name = self.title
+                        note[0].date = Date()
+                        
+                        do{
+                            try self.viewContext.save()
+                        }catch{
+                            print("TODO err")
+                        }
+                        
+                    }
+                }else{
+                    do{
+                        let note = Note(context: self.viewContext)
+                        note.content = self.text
+                        note.name = self.title
+                        note.date = Date()
+                        note.id = UUID()
+                        note.is_in = self.folder
+                        try self.viewContext.save()
+                    }catch{
+                        print("TODO: Handle err")
+                    }
                 }
+
                 
                 UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                 
             }){Text("Uložiť zmeny")})
-    }
-}
-
-struct NoteDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        NoteDetailView(folder: Folder())
     }
 }
--- NotesDemo/View/NoteView.swift
+++ NotesDemo/View/NoteView.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct NoteView: View {
     var folder: Folder
     @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest var notes: FetchedResults <Note>
+    init(folder:Folder) {
+        self.folder = folder
+        
+        self._notes = FetchRequest(entity: Note.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Note.date, ascending: true)], predicate: NSPredicate(format: "is_in = %@",folder))
+    }
     var body: some View {
         VStack {
             List{
-                ForEach(self.folder.wContains){ note in
-                    NavigationLink(destination:NoteDetailView(title: note.name ?? "Unknown", text: note.content ?? "Unknown", folder:self.folder,id: note.id)){
+                ForEach(notes){ note in
+                    NavigationLink(destination:NoteDetailView(title: note.name ?? "Unknown", text: note.content ?? "Unknown", folder:self.folder,id: note.id,changed: UUID())){
                         Text(note.name ?? "Unknown")
                     }
                 }
@@ -23,7 +29,7 @@ struct NoteView: View {
             .navigationBarItems(trailing: Button(action:{
                 
             }){
-                NavigationLink(destination: NoteDetailView(folder: self.folder)){
+                NavigationLink(destination: NoteDetailView(folder: self.folder, changed: UUID())){
                     Text("Nová poznámka")
                 }
             }
@@ -38,4 +44,6 @@ struct NoteView_Previews: PreviewProvider {
     static var previews: some View {
         NoteView(folder: Folder())
     }
+    
+
 }
