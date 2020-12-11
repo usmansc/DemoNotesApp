c3ea992a2ecfeb95a2ebcff327551a9f748f4967
Lukáš Schmelcer <xschme00@fit.vutbr.cz>
2020-12-11 17:50:49 +0100

    Working UI for Folder View + cleanup of unnecessary files

--- NotesDemo/NotesDemoApp.swift
+++ NotesDemo/NotesDemoApp.swift
@@ -13,7 +13,7 @@ struct NotesDemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FolderView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
--- NotesDemo/View/FolderView.swift
+++ NotesDemo/View/FolderView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 
 struct FolderView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: Folder.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Folder.name, ascending: true)]) var folders : FetchedResults <Folder>
+    
+    @State private var alert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack {
+                List{
+                    ForEach(folders){ folder in
+                        Text(folder.name ?? "Unknown")
+                    }
+                }
+                .navigationBarTitle("Priečinky")
+                .disabled(self.alert)
+                .navigationBarItems(trailing:
+                                        Button(action: {
+                                            self.alert.toggle()
+                                        }){
+                                            self.alert ? Text("Zavrieť") : Text("Nový priečinok")
+                                        })
+                if(alert){
+                    VStack{
+                        NewFolderAlert(shown: self.$alert)
+                            .frame(width: 300, height: 200, alignment: .center).background(Color.black).cornerRadius(30)
+                        Spacer()
+                    }.padding()
+                    
+                }
+                
+            }
+        }
     }
 }
 
 struct FolderView_Previews: PreviewProvider {
     static var previews: some View {
         FolderView()
+    }
+}
+
+struct NewFolderAlert: View{
+    @State private var text = ""
+    @Binding<Bool> var shown: Bool
+    //@EnvironmentObject var folderModel: FoldersViewModel
+    var body: some View{
+        VStack{
+            Text("Nový priečinok").foregroundColor(.white)
+            Text("Zadajte názov pre tento priečinok").foregroundColor(.white)
+            TextField("Názov", text: $text).padding().foregroundColor(.white).overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.gray))
+            Button(action: {
+                self.shown.toggle()
+                let trimmed = self.text.trimmingCharacters(in: .whitespacesAndNewlines)
+                if(trimmed.count > 0){
+                    //self.folderModel.addFolder(folder: trimmed)
+                }
+            }){
+                Text("Uložiť")
+            }
+            
+            
+        }.padding()
     }
 }
