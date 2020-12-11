91f7f8870181e8507467733273bc538810e0b7c6
Lukáš Schmelcer <xschme00@fit.vutbr.cz>
2020-12-11 17:29:11 +0100

    basic views added

---
+++ NotesDemo/View/FolderView.swift
@@ -0,0 +1,20 @@
+//
+//  FolderView.swift
+//  NotesDemo
+//
+//  Created by Lukas Schmelcer on 11/12/2020.
+//
+
+import SwiftUI
+
+struct FolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FolderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FolderView()
+    }
+}
---
+++ NotesDemo/View/NoteDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteDetailView.swift
+//  NotesDemo
+//
+//  Created by Lukas Schmelcer on 11/12/2020.
+//
+
+import SwiftUI
+
+struct NoteDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteDetailView()
+    }
+}
